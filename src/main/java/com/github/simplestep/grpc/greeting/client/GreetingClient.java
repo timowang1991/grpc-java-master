58e34020dbd7411e99f156550cb95d8b9fceb9de
Timo Wang <chiuanw@yahoo-inc.com>
2019-10-03 22:47:30 +0800

    unary greeting server and client ready

--- src/main/java/com/github/simplestep/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/github/simplestep/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,10 @@
 package com.github.simplestep.grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -9,16 +13,34 @@ public class GreetingClient {
         System.out.println("Hello I', a gRPC client");
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
                 .build();
 
         System.out.println("Creating Stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
-
-        //
+        // old and dummy
+        // DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
         //  DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
 
-        // do something
+        // created a greet service client (blocking - synchronous)
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
 
+        // created a protocol buffer greeting message
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Stephane")
+                .setLastName("Maarek")
+                .build();
+
+        // do the same for a GreetRequest
+        GreetRequest greetRequest = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        // call the RPC and get back a GreetResponse (protocol buffers)
+        GreetResponse greetResponse = greetClient.greet(greetRequest);
+
+        System.out.println("greetResponse : " + greetResponse.getResult());
+
+        // do something
         System.out.println("Shutting down channel");
         channel.shutdown();
     }
